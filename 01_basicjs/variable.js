ce97d844c0633bc19e58f972a1e3459fc3d5529e
Vinay-Kaushal <157987972+Vinay-Kaushal@users.noreply.github.com>
2024-08-07 19:10:06 +0000

    container config

---
+++ 01_basicjs/variable.js
@@ -0,0 +1,16 @@
+  const accountID = 21132
+  var accountEmail = "vinay@gmail.com"
+  let accountPassword = "124";
+  accountCity = "bengaluru"
+   
+  accountEmail = "hc@lhhlkkjn"
+  accountPassword = "0978098"
+  accountCity = "jaipur"
+
+  /*
+    problem of var keyword in earlier es5 is
+    could not know which variable is in global scope or which in functional scope
+    in javascript you can directly store the value without given the types but its seems bad practices
+  */
+
+  console.table([accountID,accountEmail,accountPassword,accountCity])
