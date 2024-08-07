df3935c3ba285d8661e6aa0ecbc386d9005ddb51
Vinay-Kaushal <157987972+Vinay-Kaushal@users.noreply.github.com>
2024-08-07 19:33:29 +0000

    datatypes basic

---
+++ 01_basicjs/datatypes.js
@@ -0,0 +1,19 @@
+  
+  let age = 12
+  let name = "vinay"
+  let isLoggedin = true
+  let state; 
+
+
+  /*
+   int/num = > 2 to pwr 53
+   bigInt => for using in like trading or replit app
+   string => ""
+   boolean => true/false
+   null => stand alone value
+   undefined => whenever declare a variable but not give or assign them value
+   
+   // non-primitive type - array, object 
+  */
+
+   console.log(typeof "vinay") // here typeof function will tell us which type of value is exactly we are pasing
