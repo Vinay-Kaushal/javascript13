3fa1bb5b2b12c624e60c0345937468c04b91ca52
Vinay-Kaushal <157987972+Vinay-Kaushal@users.noreply.github.com>
2024-08-07 20:07:51 +0000

    conversion datatpe01

---
+++ 01_basicjs/ conversion.js
@@ -0,0 +1,21 @@
+ /*
+  point 1 => to convert one type of data into another 
+ always make first letter capital of datatype eg - str to num == > Number(under this pass data or value)
+  
+  point 2 => if we declare the string and not paasing the value string 
+  rather than paasing a num while after conversing the typeof we get string not a num  
+  */
+
+  let string = "12"
+
+  let num = Number(age)
+  console.log(num)
+  console.log(typeof(num))   // here in this code the string we get converted into num but in the
+                             // below code the string converted into num but the actual type is string
+
+  /*
+  let string = "12uuyu"
+
+  let num = Number(age)
+  console.log(num)
+  console.log(typeof(num)) */
