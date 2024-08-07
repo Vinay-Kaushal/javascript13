56272aacc022e6bea3e82726701efe4d3e1a3ecd
Vinay-Kaushal <157987972+Vinay-Kaushal@users.noreply.github.com>
2024-08-07 20:56:17 +0000

    comparison

---
+++ 01_basicjs/comparison.js
@@ -0,0 +1,21 @@
+
+
+console.log(2>=1)
+console.log(2==1)
+
+//comparison bw diff datatype 
+
+console.log("2" > 1)
+console.log("07" > 1) //true
+
+console.log(null > 0)
+console.log(null == 0)//nan
+console.log( null >= 0) //while comparison this become 0 num
+
+
+console.log( undefined ==0)
+console.log ( undefined >= 0) //this become always false
+
+// ==> === 
+
+console.log( "2" === 2) // this will become false 
