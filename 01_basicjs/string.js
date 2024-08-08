8d6263b2a01a5bf4af64534bb3ae48b8462d42a1
Vinay-Kaushal <vinaykaushal87439@gmail.com>
2024-08-08 23:09:12 +0530

    strings

---
+++ 01_basicjs/string.js
@@ -0,0 +1,30 @@
+    const myName = "Vinay-hey"
+   
+    const repoCount  = 120 
+
+   console.log(`hey my name is ${myName} and my repocount is ${repoCount}`)
+
+   //this hectic is a good way to concatanate the string 
+
+   console.log(myName.length)
+
+   const anotherName = myName.charAt(4)
+   console.log(anotherName)
+
+   console.log(myName.indexOf("n"))
+
+   console.log(myName.toUpperCase())
+
+   const name1 = "  vinay "
+    
+   console.log(name1)
+   console.log(name1.trim())
+
+   console.log(myName.includes("oy"))
+
+   console.log(myName.replace("a" , "b"))
+
+   console.log(myName.slice(0 ,1))
+
+   console.log(myName.split())
+
