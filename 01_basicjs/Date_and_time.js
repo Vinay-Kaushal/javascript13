21a9d0ecdbdc056df7903678105df2d306a25c95
Vinay-Kaushal <vinaykaushal87439@gmail.com>
2024-08-09 00:38:30 +0530

    date and time

---
+++ 01_basicjs/Date_and_time.js
@@ -0,0 +1,12 @@
+
+//dates 
+
+let myDate = new Date()
+console.log(myDate)
+
+console.log(myDate.toString())
+
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+
+///and many of this date and time function
